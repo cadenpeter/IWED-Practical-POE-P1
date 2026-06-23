2e065683feffaebeb5b098b5fb0034b77084da83
cadenpeter <st10462015@rcconnect.edu.za>
2026-06-24 01:55:05 +0200

    My drawer isn't working
    AI advised me to change a bunch of my code that the lecturer taught me and there's no one i can ask for help becuase we technically don't have a lecturer. I dont knwo what to do and i dont want to fail but i dont know whats wrong with my code.

--- First POE P1/myScript.js
+++ First POE P1/myScript.js
@@ -1,23 +1,10 @@
 const menuBtn = document.getElementById("menuBtn");
 const drawer = document.getElementById("drawer");
-const datetime = document.getElementById("datetime")
 
-let isOpen = false;
+console.log(menuBtn);
+console.log(drawer);
 
-/*Mobile drawer toggle */
-menuBtn.addEventListener("click", () => {
-    if(drawer.style.display == "block"){
-        drawer.style.display = "none";
-    }else{
-        drawer.style.display = "block";
-    }
+menuBtn.addEventListener("click", function () {
+    drawer.classList.toggle("show");
 });
 
-/* Live date and time */
-function updateTime() {
-    const now = new Date ();
-    datetime.textContent = now.toLocaleString();
-}
-
-setInterval(updateTime, 1000);
-updateTime();
