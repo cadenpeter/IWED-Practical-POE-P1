7119a02e886f5217e0a3ff69d2d4cdf5ef87f1f3
cadenpeter <st10462015@rcconnect.edu.za>
2026-06-24 12:40:10 +0200

    Redid CSS MenuBtn & Drawer
    I redid my entire css for my MenuBtn and drawer

--- First POE P1/myScript.js
+++ First POE P1/myScript.js
@@ -1,10 +1,1 @@
-const menuBtn = document.getElementById("menuBtn");
-const drawer = document.getElementById("drawer");
-
-console.log(menuBtn);
-console.log(drawer);
-
-menuBtn.addEventListener("click", function () {
-    drawer.classList.toggle("show");
-});
 
