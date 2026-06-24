030c4b9c513094c78d9ba5205bbb4d51a08a9b95
cadenpeter <st10462015@rcconnect.edu.za>
2026-06-24 13:13:47 +0200

    JavaScript
    Finished my java script for my drawer and menubtn

--- First POE P1/myScript.js
+++ First POE P1/myScript.js
@@ -1,1 +1,11 @@
+const menuBtn = document.getElementById("menuBtn");
+const drawer = document.getElementById("drawer");
 
+/* Mobile drawer toggle */
+menuBtn.addEventListener("click", () => {
+    if(drawer.style.display == "block"){
+        drawer.style.display = "none";
+    }else{
+        drawer.style.display = "block";
+    }
+});
